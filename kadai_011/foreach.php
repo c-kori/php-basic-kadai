3f8a716d67850626157c035ae4733f60cda05bde
c-kori <io79135tisa@gmail.com>
2025-04-15 23:50:58 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+<?php
+$sales = [
+    '名前' => '玉ねぎ',
+    '値段' => 200,
+    '産地' => '北海道'];
+foreach ($sales as $key => $value) {
+    echo "{$key}:{$value}<br>";
+}
+?>
+</body>
+</html>
